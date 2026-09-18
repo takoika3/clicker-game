ff3a8d440391fac3799ba03b8b4a7d5583e3e9fd
takoika3 <tako12130313@gmail.com>
2026-09-18 14:19:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -351,7 +351,7 @@ document.getElementById("clickPowerGachaBtn").addEventListener("click", () => {
 
   score -= costClickPowerGacha;
 
-  const results = [100, 300, 1000, 5000, 100000];
+  const results = [1, 3, 5, 10];
   const gain = results[Math.floor(Math.random() * results.length)];
 
   clickPower += gain;
